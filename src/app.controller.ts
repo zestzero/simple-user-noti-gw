2ead82ed7103d9d6647ce6a78b53579afa67c25b
Krit Bannachaisirisuk <gmkipladen@gmail.com>
2021-07-07 01:43:36 +0700

    Initialize gateway

---
+++ src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { IResponse } from './types/response';
+
+@Controller('api')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('message')
+  getHello(): IResponse {
+    return this.appService.getHello();
+  }
+}
